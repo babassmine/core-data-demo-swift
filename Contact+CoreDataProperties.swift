070cedb80756f9eb41f5f30b895488c8465b3bc7
Abass Sesay <bassco10@gmail.com>
2015-11-15 16:06:33 -0500

    create the core data entities with attributes name, phone and address

---
+++ Contact+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Contact+CoreDataProperties.swift
+//  CoreDataDemo
+//
+//  Created by Abass Sesay on 11/15/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Contact {
+
+    @NSManaged var name: String?
+    @NSManaged var address: String?
+    @NSManaged var phone: UNKNOWN_TYPE
+    @NSManaged var status: UNKNOWN_TYPE
+
+}
---
+++ Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  CoreDataDemo
+//
+//  Created by Abass Sesay on 11/15/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
