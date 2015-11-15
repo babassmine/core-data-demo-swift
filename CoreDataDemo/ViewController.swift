fff8cd852c9f025fc5670a8c3d14e5b1ac2ada49
Abass Sesay <bassco10@gmail.com>
2015-11-15 16:04:17 -0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Abass Sesay on 11/15/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
