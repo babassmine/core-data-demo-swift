776f0795653b9b9aecb14519ff49a62497a24e7d
Abass Sesay <bassco10@gmail.com>
2015-11-15 16:16:37 -0500

    created actions and outlets

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func find(sender: AnyObject) {
+    }
+    
+    @IBAction func save(sender: AnyObject) {
+    }
+    
 
 }
 
